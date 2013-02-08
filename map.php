7acc848828909dc2fa279d7629358c69bb936e5e
elindstr <elindstr@localhost.localdomain>
2013-02-08 14:27:03 -0500

    Updated header in map

--- map.php
+++ map.php
@@ -1,8 +1,14 @@
+<!DOCTYPE html>
 <html>
-<body>
-	<?php
-		include_once 'header.php';
-	?>
-The map goes here.
-</body>
+	<head>
+		<title>BWCA:Map</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	</head>
+	<body>
+		<?php
+			include_once 'header.php';
+		?>
+	The map goes here.
+	</body>
 </html>
