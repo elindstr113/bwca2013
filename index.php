df28ee0645d0a93e73819b1e1868df42997c2921
elindstr <elindstr@localhost.localdomain>
2013-02-08 13:41:25 -0500

    Initial commit of header and index and stylesheet.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="/css/main.css"/>
+		<title>Boundary Waters 2013</title>
+	</head>
+	<body>
+		<?php
+		include_once 'header.php';
+		?>
+	</body>
+</html>
