dea9e27ddd6c1157b695b8570dc1e10fecfa5008
elindstr <elindstr@localhost.localdomain>
2013-02-08 13:41:19 -0500

    Initial commit of header and index and stylesheet.

---
+++ header.php
@@ -0,0 +1,12 @@
+<div id="header">
+	<div id="banner">
+		BWCA 2013
+	</div>
+	<div id="menubar">&nbsp;
+		<div class="menuitem">People</div>
+		<div class="menuitem">Map</div>
+		<div class="menuitem">Gear</div>
+		<div class="menuitem">Food</div>
+		<div class="menuitem">Costs</div>
+	</div>
+</div>
