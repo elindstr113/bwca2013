89b8bec5aa9c1ef717412bba4885d8c55b21bd63
Eric Lindstrom <eric.lnidstrom@comacast.net>
2013-02-11 22:38:54 -0500

    Added costs and gear pages. Modified map page.

---
+++ costs.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>BWCA2013:Costs</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	</head>
+	<body>
+		<?php
+		include_once 'header.php';
+		?>
+	</body>
+</html>
--- map.php
+++ map.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>BWCA:Map</title>
+		<title>BWCA2013:Gear</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 	</head>
