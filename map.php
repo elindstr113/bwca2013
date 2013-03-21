179584b6e1c89266c7ec150c2a31d8f88c19281e
elindstr <elindstr@Arch13>
2013-03-21 13:43:50 -0400

    Updated map to include legend.  Updated css with map specifics.

--- map.php
+++ map.php
@@ -5,14 +5,68 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 	</head>
-	<body>
+	<body style="background-color: #F0F3E6;">
 		<?php
 			include_once 'header.php';
 		?>
-            <div id="mapDiv">
-                <a href="https://maps.google.com/maps/ms?msid=210438083767512683966.0004d3af1a310d377dc5d&msa=0&ll=48.141349,-91.700821&spn=0.35556,0.836334" target="_blank">
-                <img src="images/map.png"></a>
-            </div>
+    <br/>
+    <table align="center">
+      <tr>
+        <td valign="top" class="bpad">
+          <table border="1" cellspacing="0" cellpadding="0" rules="none" id="mapLegend">
+            <tr>
+              <th colspan="3">Proposed Route</th>
+            </tr>
+            <tr>
+              <td>Begin</td>
+              <td>EP 16</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Day 1</td>
+              <td>Lake Agnes</td>
+              <td align="right">9.4 miles</td>
+            </tr>
+            <tr>
+              <td>Day 2</td>
+              <td>Iron Lake</td>
+              <td align="right">12.7 miles</td>
+            </tr>
+            <tr>
+              <td>Day 3</td>
+              <td>N. Wednesday Bay</td>
+              <td align="right">11.7 miles</td>
+            </tr>
+            <tr>
+              <td>Day 4</td>
+              <td>Horse Lake</td>
+              <td align="right">11.9 miles</td>
+            </tr>
+            <tr>
+              <td>Day 5</td>
+              <td>Angleworm Lake</td>
+              <td align="right">11.6 miles</td>
+            </tr>
+            <tr>
+              <td>Exit</td>
+              <td>EP 77</td>
+              <td align="right">4.3 miles</td>
+            </tr>
+            <tr>
+              <td style="border-top: solid black 1px;"></td>
+              <td align="right" style="border-top: solid black 1px;"><b>Total</b></td>
+              <td align="right" style="border-top: solid black 1px;">61.6 miles</td>
+            </tr>
+          </table>
+        </td>
+        <td valign="top" class="bpad">
+          <div id="mapDiv">
+            <a href="https://maps.google.com/maps/ms?msid=210438083767512683966.0004d3af1a310d377dc5d&msa=0&ll=48.141349,-91.700821&spn=0.35556,0.836334" target="_blank">
+            <img src="images/map.png"></a>
+          </div>
+        </td>
+      </tr>
+    </table>
 	
         
 	</body>
