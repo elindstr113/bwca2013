6166402e2d3050c353a2ef15f52cdba38bc8df7d
elindstr <elindstr@Arch13>
2013-03-25 08:16:19 -0400

    Created gear list.

--- gear.php
+++ gear.php
@@ -1,13 +1,53 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>BWCA2013:Gear</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" type="text/css" href="css/main.css"/>
-	</head>
-	<body>
-		<?php
-		include_once 'header.php';
-		?>
-	</body>
+  <head>
+    <title>BWCA2013:Gear</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+  </head>
+  <body>
+    <?php
+    include_once 'header.php';
+    ?>
+    <br/>
+    <table align="center" border="1" cellspacing="0" cellpadding="0" rules="none">
+      <tr><th>ITEM</th><th>QTY</th></tr>
+      <tr><td>Tee Shirts</td><td>3</td></tr>
+      <tr><td>Shorts/suit</td><td>3</td></tr>
+      <tr><td>Sweatshirt</td><td>1</td></tr>
+      <tr><td>Nylon pants</td><td>1</td></tr>
+      <tr><td>Wool socks</td><td>1</td></tr>
+      <tr><td>Beach towel</td><td>1</td></tr>
+      <tr><td>Sleeping bag</td><td>1</td></tr>
+      <tr><td>Sleeping pad</td><td>1</td></tr>
+      <tr><td>Blowup pillow</td><td>1</td></tr>
+      <tr><td>Camera</td><td>1</td></tr>
+      <tr><td>Raincoat</td><td>1</td></tr>
+      <tr><td>Headlamp/flashlight</td><td>1</td></tr>
+      <tr><td>Baby wipes</td><td>12</td></tr>
+      <tr><td>Rope</td><td>50'</td></tr>
+      <tr><td>Allergy pills</td><td>6</td></tr>
+      <tr><td>Sunscreen</td><td>1</td></tr>
+      <tr><td>Tums</td><td>8</td></tr>
+      <tr><td>Motrin (800)</td><td>8</td></tr>
+      <tr><td>Bug spray</td><td>1</td></tr>
+      <tr><td>Tooth brush</td><td></td></tr>      
+      <tr><td>Toothpaste</td><td>1</td></tr>
+      <tr><td>Sanitizer</td><td>1</td></tr>
+      <tr><td>Watch</td><td>1</td></tr>
+      <tr><td>Compass</td><td>1</td></tr>
+      <tr><td>AA batteries</td><td>2</td></tr>
+      <tr><td>Water bottle</td><td>1</td></tr>
+      <tr><td>Life jacket</td><td>1</td></tr>
+      <tr><td>Lighter</td><td>1</td></tr>
+      <tr><td>Hat</td><td>1</td></tr>
+      <tr><td>Sunglasses</td><td>1</td></tr>
+      <tr><td>Leatherman tool</td><td>1</td></tr>
+      <tr><td>Book</td><td>1</td></tr>
+      <tr><td>Fishing pole</td><td>1</td></tr>
+      <tr><td>Assorted fishing gear</td><td>1</td></tr>
+      <tr><td>Forceps</td><td>1</td></tr>
+      <tr><td>First aid kit</td><td></td></tr>
+    </table>
+  </body>
 </html>
