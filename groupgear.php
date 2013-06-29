d808b4776f1fcdae480ae7dd65daad0d8719dd8c
Eric Lindstrom <eric.lindstrom@comacast.net>
2013-06-29 18:38:36 -0400

    Update groupgear page & css.

--- groupgear.php
+++ groupgear.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="css/main.css"/>
     <style>
-      td:nth-child(1) {
+      td:nth-child(-n+3) {
         text-align:center;
       }
       </style>
@@ -17,89 +17,159 @@
     <br/>
     <table class="bodyTable" border="0" cellspacing="0" cellpadding="2" align="center" width="400px">
       <tr>
-        <th>Qty</th>
+        <th>Qty Needed</th>
+        <th>Bring</th>
+        <th>Rent</th>
         <th>Description</th>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>4</td>
+        <td></td>
+        <td>4</td>
+        <td>Canoes</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td></td>
+        <td>2</td>
+        <td>Deluxe Food Pack</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td></td>
+        <td>1</td>
+        <td>Deluxe Pack</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>1</td>
+        <td>1</td>
+        <td>Tents</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>2</td>
+        <td></td>
+        <td>Trip Maps (4 per set)</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Stove</td>
       </tr>
       <tr>
-        <td class="qty">3</td>
+        <td>3</td>
+        <td>3</td>
+        <td></td>
         <td>Propane Tanks</td>
       </tr>
       <tr>
-        <td class="qty">2</td>
+        <td>2</td>
+        <td>2</td>
+        <td></td>
         <td>Pots</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Fry Pan</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1 (church)</td>
+        <td></td>
+        <td>Cast Iron Fry Pan</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>
           Cooking Utensils
           <ul>
             <li>Big Spoon</li>
             <li>Ladle</li>
+            <li>Spatula</li>
+            <li>Metal Spatula</li>
             <li>Sharp Knife</li>
+            <li>Fillet Knife</li>
           </ul>
         </td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Dish Soap</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Dish Scubbie</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Hot Pad / OvGlove</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Cutting Board</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
-        <td>Filet Knife</td>
-      </tr>
-      <tr>
-        <td class="qty">8</td>
+        <td>8</td>
+        <td>8</td>
+        <td></td>
         <td>Plates</td>
       </tr>
       <tr>
-        <td class="qty">8</td>
+        <td>8</td>
+        <td>8</td>
+        <td></td>
         <td>Forks</td>
       </tr>
       <tr>
-        <td class="qty">2</td>
+        <td>8</td>
+        <td>8</td>
+        <td></td>
+        <td>Spoons</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>2</td>
+        <td></td>
         <td>Water Filters</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>First Aid Kit</td>
       </tr>
       <tr>
-        <td class="qty">1</td>
+        <td>1</td>
+        <td>1</td>
+        <td></td>
         <td>Duct Tape</td>
       </tr>
       <tr>
-        <td class="qty">2</td>
+        <td>3</td>
+        <td>3</td>
+        <td></td>
         <td>Toilet Paper</td>
       </tr>
       <tr>
-        <td class="qty">5</td>
+        <td>5</td>
+        <td>5</td>
+        <td></td>
         <td>Garbage Bags</td>
       </tr>
-      <tr>
-        <td class="qty">2</td>
-        <td>Tents</td>
-      </tr>
-
     </table>
   </body>
 </html>
